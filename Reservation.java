0fbb9d00ca74626641755b9c52bd15a1c26bf5a8
labhaoisemullins <77689895+labhaoisemullins@users.noreply.github.com>
2021-12-02 09:09:44 +0000

    Final Reservation.java file
    Hotel Reservation System file - Reservation

--- Reservation.java
+++ Reservation.java
@@ -1,117 +1,110 @@
-/** Java file : Reservation.java
-Team 10:
-Labhaoise Mullins (20266928)
-Aoife Kennedy (20267746)
-Gabriela Lyko (20226705)
-Kate Hickey (20266553)
-*/
-
-
-/**
- *  Reservation class creates an ArrayList of reservations using the data inputted by user.
- */
-
+import java.time.LocalDate;
 import java.util.ArrayList;
+/** Java file : Reservation.java
+ Team 10:
+ Labhaoise Mullins (20266928)
+ Aoife Kennedy (20267746)
+ Gabriela Lyko (20226705)
+ Kate Hickey (20266553)
+ */
 public class Reservation {
+
+    private int reservationNumber;
     private String name;
+    private char type;
     private String phoneNumber;
     private String email;
-    private CheckIn i;
-    private CheckOut o;
-    private String numOfRooms;
-    private String reservationNumber;
-    private char type;
-
+    private String i;
+    private String o;
+    private int numOfRooms;
+    private String roomType;
+    private int numOfPeople;
     private ArrayList<Reservation> reservations = new ArrayList<>(100);
 
 
-    public Reservation(String name,char type, String number, String email, CheckIn i, CheckOut o, String numOfRooms) {
+    /**
+     * This constructor will take input from the user and create a Reservation object based on those values
+     */
+
+    public Reservation (){
+
+    }
+
+    public Reservation(String name, char type, String phoneNumber,  String email, String i,  String o, int numOfRooms,  String roomType, int numOfPeople){
+        this.reservationNumber = (int) (Math.random() * 1000);
         this.name = name;
         this.type = type;
-        this.phoneNumber = number;
+        this.phoneNumber = phoneNumber;
         this.email = email;
         this.i = i;
         this.o = o;
         this.numOfRooms = numOfRooms;
-        this.reservationNumber = number;
+        this.roomType = roomType;
+        this.numOfPeople = numOfPeople;
+
+
     }
 
-    public void checkDetails() throws IllegalArgumentException {
-        Reservation r = new Reservation(name, type,  phoneNumber, email, i, o, numOfRooms);
-        try{
-            if (r.getName().isEmpty() || r.getType() == ' ' || r.getPhoneNumber().isEmpty() || r.getEmail().isEmpty()|| r.getCheckIn().isEmpty() || r.getCheckOut().isEmpty() || r.getNumOfRooms().isEmpty()){
-                throw new IllegalArgumentException("Invalid Details");
-            }
-        }
-        catch(IllegalArgumentException x){
-            System.out.println("Please Try Again");
-        }
-    }
-
-    public void add(Reservation r){
-        reservations.add(r);
-    }
-    public void cancel(String name){
-        for (int i = 0; i < reservations.size() ; i++)
-       if(name == reservations.get(i).getName()){
-           reservations.remove(i);
-       }
-        else{
-        System.out.println("There is no reservation under your name");
-       }
-    }
+    /**
+     * returns a reservation object as a String
+     * @return
+     */
     public String format(){
-        return getName() +", "+ getType()+", "+ getPhoneNumber()+", "+getEmail() +", "+getCheckIn() +", "+getCheckOut() +", "+getNumOfRooms();
+            return getReservationNumber() + ", " + getName() + ", " + getPhoneNumber() + ", " + getEmail() + ", " + getCheckIn() + ", " + getCheckOut() + ", " + getNumOfRooms() + ", " + getRoomType()+", " + getNumOfPeople();
+
     }
 
-    public String printSpecific(String name){
-        String found = null;
-       for (int i = 0; i < reservations.size(); i++){
-           if (reservations.get(i).getName() == name){
-                found = reservations.get(i).format();
-           }
-           else{
-               found = "Reservation does not exist";
+    public int getReservationNumber() {
 
-           }
-       }
-       return found;
-    }
-
-
-    public String getReservationNumber() {
         return reservationNumber;
     }
 
     public char getType() {
+
         return type;
     }
 
     public ArrayList<Reservation> getReservations() {
+
         return reservations;
     }
 
     public String getName() {
+
         return name;
     }
 
     public String getPhoneNumber() {
+
         return phoneNumber;
     }
 
     public String getEmail() {
+
         return email;
     }
 
     public String getCheckIn() {
-        return i.format();
+
+        return i;
     }
 
     public String getCheckOut() {
-        return o.format();
+
+        return o;
     }
 
-    public String getNumOfRooms(){
+    public int getNumOfRooms(){
+
         return numOfRooms;
     }
+
+    public String getRoomType(){
+        return roomType;
+    }
+
+    public int getNumOfPeople(){
+        return numOfPeople;
+    }
+
 }
